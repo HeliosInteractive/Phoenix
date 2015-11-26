a041e434e138bc057922aca82284c289ab0ae883
Sepehr Laal <sepehr.laal@gmail.com>
2015-11-25 16:39:58 -0800

    reflectionified stuff a bit

--- phoenix/MainDialog.cs
+++ phoenix/MainDialog.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace phoenix
@@ -22,30 +14,29 @@ namespace phoenix
             ApplySettings();
         }
 
-
         private void ApplySettings()
         {
             string section = "Local";
 
-            application_to_watch.Text               = m_AppSettings.Read(section, "ApplicationToWtach",         Defaults.ApplicationToWtach);
-            command_line_arguments.Text             = m_AppSettings.Read(section, "CommandLineArguments",       Defaults.CommandLineArguments);
-            time_delay_before_launch.Text           = m_AppSettings.Read(section, "TimeDelayBeforeLaunch",      Defaults.TimeDelayBeforeLaunch).ToString();
-            script_to_execute_on_crash.Text         = m_AppSettings.Read(section, "ScriptToExecuteOnCrash",     Defaults.ScriptToExecuteOnCrash);
-            force_always_on_top.Checked             = m_AppSettings.Read(section, "ForceAlwaysOnTop",           Defaults.ForceAlwaysOnTop);
-            force_maximize.Checked                  = m_AppSettings.Read(section, "ForceMaximize",              Defaults.ForceMaximize);
-            enable_metrics.Checked                  = m_AppSettings.Read(section, "EnableMetrics",              Defaults.EnableMetrics);
-            assume_crash_if_not_responsive.Checked  = m_AppSettings.Read(section, "AssumeCrashIfNotResponsive", Defaults.AssumeCrashIfNotResponsive);
-            start_with_windows.Checked              = m_AppSettings.Read(section, "StartWithWindows",           Defaults.StartWithWindows);
-            enable_verbose_logging.Checked          = m_AppSettings.Read(section, "EnableVerboseLogging",       Defaults.EnableVerboseLogging);
-            enable_screenshot_on_crash.Checked      = m_AppSettings.Read(section, "EnableScreenshotOnCrash",    Defaults.EnableScreenshotOnCrash);
+            application_to_watch.Text               = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.ApplicationToWtach),         Defaults.Local.ApplicationToWtach);
+            command_line_arguments.Text             = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.CommandLineArguments),       Defaults.Local.CommandLineArguments);
+            time_delay_before_launch.Text           = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.TimeDelayBeforeLaunch),      Defaults.Local.TimeDelayBeforeLaunch).ToString();
+            script_to_execute_on_crash.Text         = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.ScriptToExecuteOnCrash),     Defaults.Local.ScriptToExecuteOnCrash);
+            force_always_on_top.Checked             = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.ForceAlwaysOnTop),           Defaults.Local.ForceAlwaysOnTop);
+            force_maximize.Checked                  = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.ForceMaximize),              Defaults.Local.ForceMaximize);
+            enable_metrics.Checked                  = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.EnableMetrics),              Defaults.Local.EnableMetrics);
+            assume_crash_if_not_responsive.Checked  = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.AssumeCrashIfNotResponsive), Defaults.Local.AssumeCrashIfNotResponsive);
+            start_with_windows.Checked              = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.StartWithWindows),           Defaults.Local.StartWithWindows);
+            enable_verbose_logging.Checked          = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.EnableVerboseLogging),       Defaults.Local.EnableVerboseLogging);
+            enable_screenshot_on_crash.Checked      = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Local.EnableScreenshotOnCrash),    Defaults.Local.EnableScreenshotOnCrash);
 
             section = "Remote";
 
-            update_server_address.Text              = m_AppSettings.Read(section, "UpdateServerAddress",        Defaults.UpdateServerAddress);
-            update_channel.Text                     = m_AppSettings.Read(section, "UpdateChannel",              Defaults.UpdateChannel);
-            receive_anonymous_updates.Checked       = m_AppSettings.Read(section, "ReceiveAnonymousUpdates",    Defaults.ReceiveAnonymousUpdates);
-            update_hash.Text                        = m_AppSettings.Read(section, "UpdateHash",                 Defaults.UpdateHash);
-            username.Text                           = m_AppSettings.Read(section, "Username",                   Defaults.Username);
+            update_server_address.Text              = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Remote.UpdateServerAddress),        Defaults.Remote.UpdateServerAddress);
+            update_channel.Text                     = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Remote.UpdateChannel),              Defaults.Remote.UpdateChannel);
+            receive_anonymous_updates.Checked       = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Remote.ReceiveAnonymousUpdates),    Defaults.Remote.ReceiveAnonymousUpdates);
+            update_hash.Text                        = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Remote.UpdateHash),                 Defaults.Remote.UpdateHash);
+            username.Text                           = m_AppSettings.Read(section, Helpers.GetPropertyName(() => Defaults.Remote.Username),                   Defaults.Remote.Username);
         }
 
         private void time_delay_before_launch_KeyPress(object sender, KeyPressEventArgs e)
@@ -56,7 +47,10 @@ namespace phoenix
 
         private void application_to_watch_TextChanged(object sender, EventArgs e)
         {
-            m_AppSettings.Store("Local", "ApplicationToWtach", application_to_watch.Text);
+            m_AppSettings.Store(
+                Helpers.GetClassName(() => Defaults.Local.ApplicationToWtach),
+                Helpers.GetPropertyName(() => Defaults.Local.ApplicationToWtach),
+                (sender as TextBox).Text);
         }
     }
 }
