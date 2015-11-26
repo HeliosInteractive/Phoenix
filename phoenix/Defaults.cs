685f3a15cdbe964499a1d5475c8e642559985c97
Sepehr Laal <sepehr.laal@gmail.com>
2015-11-25 16:39:32 -0800

    put defaults inside classes to lookup section names

--- phoenix/Defaults.cs
+++ phoenix/Defaults.cs
@@ -2,26 +2,28 @@
 {
     class Defaults
     {
-        #region Local
-        public static string ApplicationToWtach         = "";
-        public static string CommandLineArguments       = "";
-        public static string ScriptToExecuteOnCrash     = "";
-        public static int TimeDelayBeforeLaunch         = 10;
-        public static bool ForceAlwaysOnTop             = false;
-        public static bool ForceMaximize                = false;
-        public static bool EnableMetrics                = true;
-        public static bool AssumeCrashIfNotResponsive   = true;
-        public static bool StartWithWindows             = true;
-        public static bool EnableVerboseLogging         = true;
-        public static bool EnableScreenshotOnCrash      = true;
-        #endregion
+        public class Local
+        {
+            public static string ApplicationToWtach = "";
+            public static string CommandLineArguments = "";
+            public static string ScriptToExecuteOnCrash = "";
+            public static int TimeDelayBeforeLaunch = 10;
+            public static bool ForceAlwaysOnTop = false;
+            public static bool ForceMaximize = false;
+            public static bool EnableMetrics = true;
+            public static bool AssumeCrashIfNotResponsive = true;
+            public static bool StartWithWindows = true;
+            public static bool EnableVerboseLogging = true;
+            public static bool EnableScreenshotOnCrash = true;
+        }
 
-        #region Remote
-        public static string UpdateServerAddress        = "";
-        public static string UpdateChannel              = "#app";
-        public static bool ReceiveAnonymousUpdates      = false;
-        public static string UpdateHash                 = "-- no hash --";
-        public static string Username                   = "";
-        #endregion
+        public class Remote
+        {
+            public static string UpdateServerAddress = "";
+            public static string UpdateChannel = "#app";
+            public static bool ReceiveAnonymousUpdates = false;
+            public static string UpdateHash = "-- no hash --";
+            public static string Username = "";
+        }
     }
 }
