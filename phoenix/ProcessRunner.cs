31a4c4d46bfb8b75f3e827d1253068ba39865baf
Sepehr Laal <sepehr.laal@gmail.com>
2015-11-26 11:46:37 -0800

    cleaned up and documented ProcessRunner

--- phoenix/ProcessRunner.cs
+++ phoenix/ProcessRunner.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace phoenix
 {
+    /// <summary>
+    /// A class that runs and monitors a given process
+    /// </summary>
     class ProcessRunner
     {
         int     m_DelaySeconds  = 0;
@@ -15,36 +18,62 @@ namespace phoenix
         string  m_CommandLine   = string.Empty;
         bool    m_Validated     = false;
 
+        /// <summary>
+        /// An event that is fired when the process is exited
+        /// Process exit code will be passed to the called handler
+        /// </summary>
+        public Action<int> ProcessExited;
+
+        /// <summary>
+        /// A script to be called in case of process exit/crash.
+        /// Process exit code will be passed to the called script
+        /// </summary>
         public string CrashScript
         {
             get { return m_CrashScript; }
             set { m_CrashScript = value; Validate(); }
         }
 
+        /// <summary>
+        /// Maximum attempts the process will be restarted before we
+        /// give up restarting it. 0 means infinite attempts.
+        /// </summary>
         public int Attempts
         {
             get { return m_Attempts; }
             set { m_Attempts = value; Validate(); }
         }
 
+        /// <summary>
+        /// The time delay before re-launching the process (seconds)
+        /// </summary>
         public int DelaySeconds
         {
             get { return m_DelaySeconds; }
             set { m_DelaySeconds = value; Validate(); }
         }
         
+        /// <summary>
+        /// Process path (could be an executable or a bat file for example)
+        /// </summary>
         public string ProcessPath
         {
             get { return m_ProcessPath; }
             set { m_ProcessPath = value; Validate(); }
         }
         
+        /// <summary>
+        /// Command line arguments to be passed to the process
+        /// </summary>
         public string CommandLine
         {
             get { return m_CommandLine; }
             set { m_CommandLine = value; Validate(); }
         }
 
+        /// <summary>
+        /// Call this method to reset the counter and monitor the process
+        /// </summary>
         public void Run()
         {
             if (!m_Validated) return;
@@ -53,6 +82,20 @@ namespace phoenix
             ExecuteProcess();
         }
 
+        /// <summary>
+        /// Virtual method, gets called on process exit every time it exits
+        /// </summary>
+        /// <param name="exit_code">Process' exit code</param>
+        protected virtual void OnProcessExit(int exit_code)
+        {
+            if (ProcessExited != null)
+                ProcessExited(exit_code);
+        }
+
+        /// <summary>
+        /// Validate members. Sets a flag that prevents Run() from operating
+        /// if members are incorrectly set.
+        /// </summary>
         void Validate()
         {
             m_Validated = false;
@@ -60,10 +103,7 @@ namespace phoenix
             m_DelaySeconds = Math.Abs(m_DelaySeconds);
             m_Attempts = Math.Abs(m_Attempts);
 
-            if (m_ProcessPath == string.Empty)
-                return;
-
-            if (!Path.IsPathRooted(m_ProcessPath))
+            if (m_ProcessPath != string.Empty && !Path.IsPathRooted(m_ProcessPath))
                 m_ProcessPath = Path.GetFullPath(m_ProcessPath);
 
             if (m_CrashScript != string.Empty && !Path.IsPathRooted(m_CrashScript))
@@ -74,6 +114,7 @@ namespace phoenix
                 m_ProcessPath = string.Empty;
                 return;
             }
+
             if (!File.Exists(m_CrashScript))
             {
                 m_CrashScript = string.Empty;
@@ -82,41 +123,56 @@ namespace phoenix
             m_Validated = true;
         }
 
+        /// <summary>
+        /// Executes the given process with set command line arguments
+        /// </summary>
         void ExecuteProcess()
         {
             if (!m_Validated) return;
 
-            ProcessStartInfo psi = new ProcessStartInfo();
-            psi.FileName = ProcessPath;
+            ProcessStartInfo process_info = new ProcessStartInfo();
+            process_info.FileName = ProcessPath;
 
             if (CommandLine != string.Empty)
-                psi.Arguments = CommandLine;
+                process_info.Arguments = CommandLine;
 
             Process process = new Process();
-            process.StartInfo = psi;
+            process.StartInfo = process_info;
             process.EnableRaisingEvents = true;
-            process.Exited += new EventHandler(ExecuteProcessAgain);
-
+            process.Exited += new EventHandler(HandleProcessExit);
             process.Start();
         }
 
-        void ExecuteProcessAgain(object sender, EventArgs e)
+        /// <summary>
+        /// Method that is called on Process exit event on .NET framework's
+        /// Process.Exited event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void HandleProcessExit(object sender, EventArgs e)
         {
             if (!m_Validated || (m_Attempts > 0 &&  m_CurrAttempt >= m_Attempts))
                 return;
 
             if (m_CrashScript != string.Empty)
             {
-                ProcessStartInfo psi = new ProcessStartInfo();
-                psi.FileName = CrashScript;
+                ProcessStartInfo process_info = new ProcessStartInfo();
+                process_info.FileName = CrashScript;
+
+                if (sender as Process != null)
+                {
+                    Process dead_process = (sender as Process);
+                    process_info.Arguments = dead_process.ExitCode.ToString();
+                    OnProcessExit(dead_process.ExitCode);
+                }
 
                 Process process = new Process();
-                process.StartInfo = psi;
+                process.StartInfo = process_info;
                 process.Start();
             }
 
             Task.Delay(1000 * DelaySeconds)
-                .ContinueWith(fn => ExecuteProcess() );
+                .ContinueWith( fn => ExecuteProcess() );
 
             m_CurrAttempt++;
         }
