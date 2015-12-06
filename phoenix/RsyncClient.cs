2fa3afc29a9e62e3ed6be40b06a21857c042a734
Sepehr Laal <sepehr.laal@gmail.com>
2015-12-05 23:31:37 -0800

    Added key generation and execution methods in

--- phoenix/RsyncClient.cs
+++ phoenix/RsyncClient.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Diagnostics;
 using System.IO.Compression;
 
 namespace phoenix
@@ -34,6 +36,17 @@ namespace phoenix
                 return files_exist;
             }
         }
+        public static bool KeysExist
+        {
+            get
+            {
+                bool keys_exist =
+                    File.Exists(string.Format("{0}{1}", ClientDirectory, Environment.MachineName)) &&
+                    File.Exists(string.Format("{0}{1}.pub", ClientDirectory, Environment.MachineName));
+
+                return keys_exist;
+            }
+        }
         public static void Extract()
         {
             if (!Directory.Exists(ClientDirectory))
@@ -56,6 +69,83 @@ namespace phoenix
                         entry.ExtractToFile(extract_path, true);
                     }
                 }
+            }
+        }
+
+        public static void GenerateKey()
+        {
+            if (!ClientExtracted)
+                Extract();
+
+            ProcessStartInfo process_info = new ProcessStartInfo();
+            process_info.Arguments = string.Format("-q -t rsa -f '{0}' -N ''", Environment.MachineName);
+            process_info.FileName = string.Format("{0}ssh-keygen.exe", ClientDirectory);
+            process_info.WorkingDirectory = ClientDirectory;
+            process_info.UseShellExecute = false;
+            process_info.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = process_info;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        public static void Execute(string remote, string local, string username, string address, short port)
+        {
+            if (!ClientExtracted)
+                Extract();
+
+            if (!KeysExist)
+                GenerateKey();
+
+            // Make sure we have an established home directory for SSH
+            string home_directory = string.Format("{0}home", ClientDirectory);
+
+            if (!Directory.Exists(home_directory))
+                Directory.CreateDirectory(home_directory);
+
+            ProcessStartInfo start_info = new ProcessStartInfo();
+            start_info.FileName = string.Format("{0}rsync.exe", ClientDirectory);
+            start_info.EnvironmentVariables["HOME"] = home_directory;
+            start_info.EnvironmentVariables["PATH"] = ClientDirectory;
+            start_info.WorkingDirectory = ClientDirectory;
+            start_info.UseShellExecute = false;
+            start_info.Arguments = string.Format(
+                "-ravz -e \"ssh -p {0} -i '{1}' -o StrictHostKeyChecking=no\" {2}@{3}:{4} {5}",
+                port,
+                Environment.MachineName,
+                username,
+                address,
+                remote,
+                local);
+            start_info.RedirectStandardError = true;
+            start_info.RedirectStandardOutput = true;
+            start_info.CreateNoWindow = true;
+
+            using (Process rsync_process = new Process())
+            {
+                rsync_process.StartInfo = start_info;
+                rsync_process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        Debug.WriteLine(e.Data);
+                    }
+                });
+                rsync_process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        Debug.WriteLine(e.Data);
+                    }
+                });
+                rsync_process.Start();
+                rsync_process.BeginOutputReadLine();
+                rsync_process.BeginErrorReadLine();
             }
         }
     }
