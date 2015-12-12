95932b9e8fcc985f7430fbd499197fc347ed14fb
Sepehr Laal <sepehr.laal@gmail.com>
2015-12-11 21:41:05 -0800

    just a wrapper to simplify my life

---
+++ phoenix/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace phoenix
+{
+    class Logger
+    {
+        public static void Info(string msg)
+        {
+            Trace.TraceInformation(msg);
+        }
+        public static void Warn(string msg)
+        {
+            Trace.TraceWarning(msg);
+        }
+        public static void Error(string msg)
+        {
+            Trace.TraceError(msg);
+        }
+    }
+}
