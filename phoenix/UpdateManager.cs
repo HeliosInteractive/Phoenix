36d80a93ea174e5797fdb2f38ffd1c19590eddb7
Sepehr Laal <sepehr.laal@gmail.com>
2016-01-26 15:40:07 -0800

    made everything async here. need to make it reenter-able as well

--- phoenix/UpdateManager.cs
+++ phoenix/UpdateManager.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Xml;
 using System.Reflection;
+using System.ComponentModel;
 
 namespace phoenix
 {
@@ -11,14 +12,14 @@ namespace phoenix
         Version m_CurrentVersion;
         Version m_UpdateVersion;
         Uri     m_UpdateAddress;
-        string  m_FeedAddress;
+        Uri     m_FeedAddress;
         string  m_FeedChannel;
 
         public UpdateManager(string feed_address)
         {
             m_CurrentVersion = Assembly.GetExecutingAssembly().GetName().Version;
             m_UpdateVersion  = Version.Parse("0.0.0.0");
-            m_FeedAddress    = feed_address;
+            m_FeedAddress    = new Uri(feed_address);
             m_FeedChannel    = "alpha"; // change this if you are working on a different branch.
 
             Logger.Info(string.Format("Update Manager is up with feed address: {0} on channel: {1}",
@@ -36,107 +37,150 @@ namespace phoenix
             Logger.Info("Checking for updates...");
             XmlDocument feed_xml = new XmlDocument();
 
-            try {
-                feed_xml.Load(m_FeedAddress);
-            } catch (XmlException) {
-                Logger.Error("Unable to load the feed xml due to malformation.");
-                feed_xml = null;
-            } catch {
-                Logger.Error("Unable to load the feed xml.");
-                feed_xml = null;
-            }
-
-            if (feed_xml == null || !feed_xml.HasChildNodes)
-                return;
-
-            try
+            using (WebClient wc = new WebClient())
             {
-                if (feed_xml["phoenix"]["channel"].InnerText == m_FeedChannel)
-                {
-                    Logger.Info(string.Format("Found a matching update channel: {0}", m_FeedChannel));
-
-                    m_UpdateVersion = Version.Parse(feed_xml["phoenix"]["version"].InnerText);
-                    Logger.Info(string.Format("Update version is parsed to be: {0}", m_UpdateVersion));
-
-                    m_UpdateAddress = new Uri(feed_xml["phoenix"]["address"].InnerText);
-                    Logger.Info(string.Format("Update address is loaded to be: {0}", m_UpdateAddress));
-                }
-                else
-                {
-                    Logger.Warn(string.Format("Update channels do not match: {0} vs {1}"
-                        , feed_xml["phoenix"]["channel"].InnerText
-                        , m_FeedChannel));
-                }
-            }
-            catch
-            {
-                Logger.Error("Update feed has missing components.");
-                m_UpdateAddress = null;
-            }
-
-            if (m_UpdateAddress == null)
-                return;
-
-            if (m_UpdateVersion > m_CurrentVersion)
-            {
-                string temp_loc = Path.Combine(
-                    Path.GetTempPath(),
-                    Guid.NewGuid().ToString(),
-                    Path.GetFileName(m_UpdateAddress.ToString()));
-
-                Logger.Info(string.Format("Downloading new phoenix to: {0}.",
-                    temp_loc));
-
-                using (WebClient client = new WebClient())
-                {
-                    Directory.CreateDirectory(Path.GetDirectoryName(temp_loc));
-
-                    try {
-                        client.DownloadFile(m_UpdateAddress, temp_loc);
+                wc.DownloadStringCompleted += (object sender, DownloadStringCompletedEventArgs e) => {
+                    try
+                    {
+                        feed_xml.LoadXml(e.Result);
                     }
-                    catch(WebException) {
-                        Logger.Error("Phoenix update download failed due to WebException.");
+                    catch (XmlException)
+                    {
+                        Logger.Error("Unable to load the feed xml due to malformation.");
+                        feed_xml = null;
+                    }
+                    catch
+                    {
+                        Logger.Error("Unable to load the feed xml.");
+                        feed_xml = null;
+                    }
+
+                    if (feed_xml == null || !feed_xml.HasChildNodes)
                         return;
-                    }
-                    catch {
-                        Logger.Error("Phoenix update download failed due to exception.");
-                        return;
-                    }
 
-                    string oldapp_location = Assembly.GetExecutingAssembly().Location;
-                    string backup_location = Path.Combine(Path.GetDirectoryName(oldapp_location), "backup.dat");
+                    try
+                    {
+                        if (feed_xml["phoenix"]["channel"].InnerText == m_FeedChannel)
+                        {
+                            Logger.Info(string.Format("Found a matching update channel: {0}", m_FeedChannel));
 
-                    Logger.Info(string.Format("Old location is: {0}. Backup location is: {1}"
-                        , oldapp_location
-                        , backup_location));
+                            m_UpdateVersion = Version.Parse(feed_xml["phoenix"]["version"].InnerText);
+                            Logger.Info(string.Format("Update version is parsed to be: {0}", m_UpdateVersion));
 
-                    if (File.Exists(backup_location)) {
-                        try {
-                            File.Delete(backup_location);
-                        } catch {
-                            Logger.Error("Unable to remove old backup.");
-                            return;
+                            m_UpdateAddress = new Uri(feed_xml["phoenix"]["address"].InnerText);
+                            Logger.Info(string.Format("Update address is loaded to be: {0}", m_UpdateAddress));
+                        }
+                        else
+                        {
+                            Logger.Warn(string.Format("Update channels do not match: {0} vs {1}"
+                                , feed_xml["phoenix"]["channel"].InnerText
+                                , m_FeedChannel));
                         }
                     }
+                    catch
+                    {
+                        Logger.Error("Update feed has missing components.");
+                        m_UpdateAddress = null;
+                    }
 
-                    try {
-                        File.Move(oldapp_location, backup_location);
-                    } catch {
-                        Logger.Error("Backup creation failed.");
+                    if (m_UpdateAddress == null)
                         return;
-                    }
 
-                    try {
-                        File.Move(temp_loc, oldapp_location);
-                    } catch {
-                        Logger.Error("Applying updates failed, rolling back.");
-                        File.Move(backup_location, oldapp_location);
+                    if (m_UpdateVersion > m_CurrentVersion)
+                    {
+                        string temp_loc = Path.Combine(
+                            Path.GetTempPath(),
+                            Guid.NewGuid().ToString(),
+                            Path.GetFileName(m_UpdateAddress.ToString()));
+
+                        Logger.Info(string.Format("Downloading new phoenix to: {0}.",
+                            temp_loc));
+
+                        using (WebClient client = new WebClient())
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(temp_loc));
+                            client.DownloadFileCompleted += (sender2, e2) =>
+                            {
+                                string oldapp_location = Assembly.GetExecutingAssembly().Location;
+                                string backup_location = Path.Combine(Path.GetDirectoryName(oldapp_location), "backup.dat");
+
+                                Logger.Info(string.Format("Old location is: {0}. Backup location is: {1}"
+                                    , oldapp_location
+                                    , backup_location));
+
+                                if (File.Exists(backup_location))
+                                {
+                                    try
+                                    {
+                                        File.Delete(backup_location);
+                                        Logger.Info("Old backup executable removed.");
+                                    }
+                                    catch
+                                    {
+                                        Logger.Error("Unable to remove old backup.");
+                                        return;
+                                    }
+                                }
+
+                                try
+                                {
+                                    File.Move(oldapp_location, backup_location);
+                                    Logger.Info("Backup executable created.");
+                                }
+                                catch
+                                {
+                                    Logger.Error("Backup creation failed.");
+                                    return;
+                                }
+
+                                try
+                                {
+                                    File.Move(temp_loc, oldapp_location);
+                                    Logger.Info("Updates successfully applied");
+                                }
+                                catch
+                                {
+                                    Logger.Error("Applying updates failed, rolling back.");
+                                    File.Move(backup_location, oldapp_location);
+                                }
+                            };
+
+                            try
+                            {
+                                client.DownloadFileAsync(m_UpdateAddress, temp_loc);
+                            }
+                            catch (WebException)
+                            {
+                                Logger.Error("Phoenix update download failed due to WebException.");
+                                return;
+                            }
+                            catch
+                            {
+                                Logger.Error("Phoenix update download failed due to exception.");
+                                return;
+                            }
+                        }
                     }
+                    else
+                    {
+                        Logger.Info("Phoenix is already a newer version.");
+                    }
+                };
+
+                try
+                {
+                    wc.DownloadStringAsync(m_FeedAddress);
                 }
-            }
-            else
-            {
-                Logger.Info("Phoenix is already a newer version.");
+                catch (WebException)
+                {
+                    Logger.Error("Phoenix feed download failed due to WebException.");
+                    return;
+                }
+                catch
+                {
+                    Logger.Error("Phoenix feed download failed due to exception.");
+                    return;
+                }
             }
         }
     }
