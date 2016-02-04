63e1a7a6f811c58b1022305d9d44eb29f03b4e8f
Sepehr Laal <sepehr.laal@gmail.com>
2016-02-04 14:54:41 -0800

    append echo'ed machines to the end of Cake's table

--- cms/webroot/js/base.js
+++ cms/webroot/js/base.js
@@ -1,12 +1,46 @@
+function BaseChannelPath() {
+	var base_channel = "/helios/phoenix"
+	return base_channel;
+}
+
+function SubChannelPath(path) {
+	return BaseChannelPath() + "/" + path;
+}
+
+function AddMachineEntry(payload, columns) {
+	try { var msg = $.parseJSON(payload); }
+	catch(e) { console.log("Failed to parse: " + payload); return; }
+	
+	var row = $('.content table')
+			.find('tbody')
+			.append($('<tr>'));
+	
+	columns.forEach(function(col) {
+		row.append($('<td>').text((col in msg) ? msg[col] : ''));
+	});
+}
+
 $(document).ready(function() {
-	console.log("here");
+	var columns = [];
+	var thead = $('.content table thead tr th')
+		.each(function( index, value )
+		{
+			var header = $('a', value).length == 1 ? $('a', value).text() : $(value).text();
+			header = header.toLowerCase().replace(' ', '_');
+			columns.push(header);
+		});
+	
 	var client = mqtt.connect("ws://test.mosquitto.org:8080/");
-	client.subscribe("/helios/phoenix");
+	client.subscribe(SubChannelPath("machines"));
 
 	client.on("message", function(topic, payload) {
-		console.log([topic, payload].join(": "));
+		if (topic == SubChannelPath("machines"))
+			AddMachineEntry(payload, columns);
+		else
+			console.log([topic, payload].join(" : "));
+		
 		client.end();
 	});
 
-	client.publish("/helios/phoenix", "hi!");
+	client.publish(BaseChannelPath(), "echo");
 });
