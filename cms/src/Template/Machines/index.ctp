e84c485c6f5659692815835a860ecbe0b3ab7b69
Sepehr Laal <sepehr.laal@gmail.com>
2016-02-10 13:18:38 -0800

    implemented the ping mechanism in cms side

--- cms/src/Template/Machines/index.ctp
+++ cms/src/Template/Machines/index.ctp
@@ -15,16 +15,18 @@
     <table cellpadding="0" cellspacing="0">
         <thead>
             <tr>
-                <th><?= $this->Paginator->sort('name') ?></th>
-                <th><?= $this->Paginator->sort('public_key') ?></th>
+                <th><?= __('Name') ?></th>
+				<th><?= __('Status') ?></th>
+                <th><?= __('Public Key') ?></th>
                 <th class="actions"><?= __('Actions') ?></th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($machines as $machine): ?>
             <tr>
-                <td><?= h($machine->name) ?></td>
-                <td><?= h($machine->public_key) ?></td>
+                <td class="name"><?= h($machine->name) ?></td>
+                <td class="status"></td>
+                <td class="public_key"><?= h($machine->public_key) ?></td>
                 <td class="actions">
                     <?= $this->Html->link(__('View'), ['action' => 'view', $machine->id]) ?>
                     <?= $this->Html->link(__('Edit'), ['action' => 'edit', $machine->id]) ?>
--- cms/webroot/js/base.js
+++ cms/webroot/js/base.js
@@ -1,3 +1,4 @@
+// console.log polyfill
 window.log = function() {
 	log.history = log.history || [];
 	log.history.push(arguments);
@@ -16,7 +17,7 @@ function SubChannelPath(path) {
 }
 
 function AuthorizeMachine(info){
-	$.post("/machines/add", info, function(data) {
+	$.post(cake.base_url + "machines/add", info, function(data) {
 		window.location.reload();
 	});
 }
@@ -34,6 +35,8 @@ function AddMachineEntry(payload, columns) {
 					.click(function() { AuthorizeMachine(msg); return false; })
 					.text('Authorize'))
 				);
+			} else if (col == "status") {
+				row.append($('<td>').text("Authorize this machine to see its stat."));
 			} else {
 				row.append($('<td>').text((col in msg) ? msg[col] : ''));
 			}
@@ -56,27 +59,81 @@ function MachineAuthorized(obj) {
 	return found;
 }
 
+function HandlePing(payload) {
+	try { var msg = $.parseJSON(payload); }
+	catch(e) { log("Failed to parse: " + payload); return; }
+	
+	$('.content table tbody tr')
+		.each(function( index, value ) {
+			var elem = $(value);
+			if (elem.text().length == 0)
+				return;
+			
+			if ($(".name", elem).text() == msg.name) {
+				var status = "<b>online</b>";
+				status += "<br />cpu: " + (msg.cpu * 100).toFixed(2) + "%";
+				status += "<br />mem: " + (msg.mem * 100).toFixed(2) + "%";
+				$(".status", elem).html(status);
+				$(elem).attr("ping", Date.now());
+			}
+		});
+	
+	console.log(msg);
+}
+
+function HandleOfflines(dead_interval) {
+	$('.content table tbody tr')
+		.each(function( index, value ) {
+			var elem = $(value);
+			if (elem.text().length == 0)
+				return;
+			
+			var last_ping = $(elem).attr("ping");
+			if (typeof last_ping !== typeof undefined && last_ping !== false) {
+				last_ping = +last_ping;
+			} else {
+				last_ping = 0;
+			}
+			
+			if (Date.now() - last_ping >= dead_interval)
+				$(".status", elem).html("<b>offline</b>");
+		});
+}
+
 $(document).ready(function() {
 	var columns = [];
 	var thead = $('.content table thead tr th')
-		.each(function( index, value )
-		{
+		.each(function( index, value ) {
 			var header = $('a', value).length == 1 ? $('a', value).text() : $(value).text();
 			header = header.toLowerCase().replace(' ', '_');
 			columns.push(header);
 		});
 	
-	var client = mqtt.connect("ws://192.168.56.1:8080/");
-	client.subscribe(SubChannelPath("machines"));
+	var client 			= mqtt.connect("ws://192.168.56.1:8080/");
+	var echo_channel 	= SubChannelPath("echo");
+	var ping_channel 	= SubChannelPath("ping");
+	var ping_interval 	= 3 * 1000;
+	var dead_interval 	= 5 * ping_interval;
+	
+	client.subscribe(echo_channel);
+	client.subscribe(ping_channel);
 
 	client.on("message", function(topic, payload) {
-		if (topic == SubChannelPath("machines"))
+		if (topic == echo_channel)
 			AddMachineEntry(payload, columns);
-		else
-			log([topic, payload].join(" : "));
-		
-		client.end();
+		else if (topic == ping_channel)
+			HandlePing(payload);
 	});
 
 	client.publish(BaseChannelPath(), "echo");
+	client.publish(BaseChannelPath(), "ping");
+	HandleOfflines(dead_interval);
+	
+	setInterval(function() {
+		client.publish(BaseChannelPath(), "ping");
+	}, ping_interval);
+	
+	setInterval(function() {
+		HandleOfflines(dead_interval);
+	}, ping_interval);
 });
