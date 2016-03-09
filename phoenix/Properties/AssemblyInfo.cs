523d4630d3941e55cf10b6c8c286f1b2cb13e9a7
Sepehr Laal <sepehr.laal@gmail.com>
2016-03-08 20:57:01 -0800

    some finessing in project settings

--- phoenix/Properties/AssemblyInfo.cs
+++ phoenix/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("phoenix")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Application Monitoring")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Helios Interactive")]
 [assembly: AssemblyProduct("phoenix")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
