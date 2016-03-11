be22edc0fb34c6a232408d3de2af07c81b2865da
Sepehr Laal <sepehr.laal@gmail.com>
2016-03-11 13:09:11 -0800

    reduced some fur off of ProcessRunner and documented it for Doxygen

--- phoenix/ProcessRunner.cs
+++ phoenix/ProcessRunner.cs
@@ -7,45 +7,62 @@
     using System.Threading.Tasks;
     using Microsoft.VisualBasic.Devices;
 
+    /// <summary>
+    /// The central nerve system of Phoenix! Handles restarting,
+    /// closing, monitoring, and etc. of a running Process.
+    /// </summary>
     class ProcessRunner : IDisposable
     {
         #region Private Members
 
-        PerformanceCounter      m_PerfCounter   = null;
+        /// <summary>Performance counter used to collect CPU usage</summary>
+        PerformanceCounter      m_CpuCounter    = null;
+        /// <summary>Process instance currently being monitored</summary>
         Process                 m_Process       = null;
+        /// <summary>Working directory of m_Process</summary>
         string                  m_WorkingDir    = string.Empty;
+        /// <summary>Absolute path to a script launched upon (re)start of m_Process</summary>
         string                  m_StartScript   = string.Empty;
+        /// <summary>Absolute path to a script launched upon crash/exit of m_Process</summary>
         string                  m_CrashScript   = string.Empty;
+        /// <summary>Absolute path to the executable of m_Process</summary>
         string                  m_ProcessPath   = string.Empty;
+        /// <summary>Command line passed to m_Process upon its launch</summary>
         string                  m_CommandLine   = string.Empty;
+        /// <summary>Cached name of m_Process (to be used for force killing m_Process)</summary>
         string                  m_CachedName    = string.Empty;
+        /// <summary>Environment variables to be merged with system's variables for m_Process</summary>
         string                  m_Environment   = string.Empty;
+        /// <summary>Memory usage samples</summary>
         double[]                m_MemoryUsage   = new double[m_NumSamples];
+        /// <summary>CPU usage samples</summary>
         double[]                m_CpuUsage      = new double[m_NumSamples];
-        double[]                m_UsageIndices  = new double[m_NumSamples];
-        double                  m_MaxMemory     = 1d;
-        double                  m_LastCpuUsage  = 0d,
-                                m_LastMemUsage  = 0d;
+        /// <summary>Maximum memory available on this machine (bytes)</summary>
+        double                  m_MaxMemory     = -1d;
+        /// <summary>Number of Metrics samples collected in m_MemoryUsage and m_CpuUsage</summary>
         const int               m_NumSamples    = 100;
+        /// <summary>The time delay (s) to wait between successive (re)starts</summary>
         int                     m_DelaySeconds  = 0;
+        /// <summary>The time delay (s) to wait before assuming unresponsive m_Process is crashed</summary>
         int                     m_WaitTime      = 0;
+        /// <summary>Keeps m_Process always on top (force-fully, works with Full-screen windows as well)</summary>
         bool                    m_AlwaysOnTop   = false;
+        /// <summary>Assume m_Process is crashed if its main window is unresponsive</summary>
         bool                    m_CrashIfUnresp = false;
+        /// <summary>Sets to be true if m_Process is being monitored or ProcessRunner is idle</summary>
         bool                    m_Monitoring    = false;
+        /// <summary>If true, stdout and stderr of m_Process is captured into Phoenix' logger</summary>
         bool                    m_CaptureOutput = false;
-        public enum ExecType    { CRASHED, NORMAL }
 
         #endregion
 
+        //! @cond
         #region Property Indexers
 
-        public double[] UsageIndices    { get { return m_UsageIndices; } }
         public double[] MemoryUsage     { get { return m_MemoryUsage; } }
         public double[] CpuUsage        { get { return m_CpuUsage; } }
         public int      NumSamples      { get { return m_NumSamples; } }
         public bool     Monitoring      { get { return m_Monitoring; } }
-        public double   LastCpuUsage    { get { return Monitoring ? m_LastCpuUsage : 0d; } }
-        public double   LastMemUsage    { get { return Monitoring ? m_LastMemUsage : 0d; } }
         public string   Environment     { get; set; }
         public bool CaptureConsoleOutput
         {
@@ -104,12 +121,21 @@
         }
 
         #endregion
+        //! @endcond
 
         #region Events
-
+        
+        /// <summary>
+        /// Event which is fired upon m_Process' (re)start
+        /// </summary>
         public Action<ExecType> ProcessStarted;
+
+        /// <summary>
+        /// Event which is fired upon m_Process' crash/exit
+        /// </summary>
         public Action<ExecType> ProcessStopped;
 
+        //! @cond
         void OnProcessStarted(ExecType type)
         {
             ResetPerformanceCounter();
@@ -125,14 +151,14 @@
         {
             m_Monitoring = false;
 
-            if (type == ExecType.CRASHED)
+            if (type == ExecType.Crashed)
             {
-                CallScript(m_CrashScript);
+                ExecuteScript(m_CrashScript, string.Empty, false);
 
                 Task.Delay(new TimeSpan(0, 0, DelaySeconds))
                     .ContinueWith((fn) => {
                         if (!m_Monitoring)
-                            Start(ExecType.CRASHED);
+                            Start(ExecType.Crashed);
                     });
             }
 
@@ -140,26 +166,47 @@
                 ProcessStopped(type);
         }
 
-        #endregion
-
-        public ProcessRunner()
-        {
-            for (int index = 0; index < m_NumSamples; ++index)
-            {
-                m_UsageIndices[index] = index;
-                m_MemoryUsage[index] = 0;
-                m_CpuUsage[index] = 0;
-            }
-
-            try { m_MaxMemory = new ComputerInfo().AvailablePhysicalMemory; }
-            catch { m_MaxMemory = -1d; }
-        }
-
         // This is here solely because I need to remove this
         // subscriber in case of a NORMAL Stop() request.
         void OnProcessCrashed(object sender, EventArgs e)
-        { OnProcessStopped(ExecType.CRASHED); }
+        {
+            OnProcessStopped(ExecType.Crashed);
+        }
 
+        //! @endcond
+
+        #endregion
+
+        /// <summary>
+        /// Enum, determining the type of start/stop of m_Process. In case of Crashed
+        /// then all subsequent callbacks are called. In case of Normal, operations
+        /// are performed silently.
+        /// </summary>
+        public enum ExecType { Crashed, Normal }
+
+        /// <summary>
+        /// Initializes metric sample arrays and tries to obtain available system memory
+        /// </summary>
+        public ProcessRunner()
+        {
+            m_MemoryUsage = Enumerable.Repeat(0d, NumSamples).ToArray();
+            m_CpuUsage = Enumerable.Repeat(0d, NumSamples).ToArray();
+
+            try
+            {
+                m_MaxMemory = new ComputerInfo().AvailablePhysicalMemory;
+            }
+            catch (Exception ex)
+            {
+                Logger.ProcessRunner.ErrorFormat("Unable to obtain available memory: {0}", ex.Message);
+                m_MaxMemory = -1d;
+            }
+        }
+
+        /// <summary>
+        /// Stops m_Process from running. No-op if already stopped.
+        /// </summary>
+        /// <param name="type">stop type (crashed / normal close)</param>
         public void Stop(ExecType type)
         {
             if (m_Process != null)
@@ -190,7 +237,7 @@
                     {
                         Logger.ProcessRunner.Warn("Failed to close with Kill API");
                         ExecuteScript("taskkill",
-                            string.Format("/F /T /IM {0}.exe", m_Process.ProcessName));
+                            string.Format("/F /T /IM {0}.exe", m_Process.ProcessName), true);
                     }
 
                     m_Process.Dispose();
@@ -210,20 +257,24 @@
                     Logger.ProcessRunner.Warn("A leftover process found with the cached name.");
 
                     ExecuteScript("taskkill",
-                        string.Format("/F /T /IM {0}.exe", m_CachedName));
+                        string.Format("/F /T /IM {0}.exe", m_CachedName), true);
                 }
             }
 
             OnProcessStopped(type);
         }
 
+        /// <summary>
+        /// Starts m_Process from running. No-op if already running.
+        /// </summary>
+        /// <param name="type">start type (after crash / normal start)</param>
         public void Start(ExecType type)
         {
             if (!Validate() || m_Disposed)
                 return;
 
-            Stop(ExecType.NORMAL);
-            CallScript(m_StartScript);
+            Stop(ExecType.Normal);
+            ExecuteScript(m_StartScript, string.Empty, false);
 
             m_Process = new Process {
                 StartInfo = new ProcessStartInfo
@@ -297,10 +348,13 @@
             {
                 Logger.ProcessRunner.ErrorFormat("Unable to start the process: {0}"
                     , ex.Message);
-                Stop(ExecType.NORMAL);
+                Stop(ExecType.Normal);
             }
         }
 
+        /// <summary>
+        /// Performs monitoring routines (check for crash and etc.)
+        /// </summary>
         public void Monitor()
         {
             if (!Monitorable())
@@ -312,7 +366,7 @@
                 Task.Delay(new TimeSpan(0, 0, WaitTime))
                     .ContinueWith((fn) => {
                         if (!m_Process.Responding)
-                            Stop(ExecType.CRASHED);
+                            Stop(ExecType.Crashed);
                     });
 
             if (ForceAlwaysOnTop && HasMainWindow())
@@ -333,6 +387,9 @@
             }
         }
 
+        /// <summary>
+        /// Collects new metric samples of m_Process
+        /// </summary>
         public void UpdateMetrics()
         {
             if (!Monitorable())
@@ -346,48 +403,58 @@
 
             int sample_index = m_NumSamples - 1;
             m_MemoryUsage[sample_index] = m_Process.WorkingSet64 / m_MaxMemory;
-            try { m_CpuUsage[sample_index] = m_PerfCounter.NextValue() / (System.Environment.ProcessorCount * 100d); }
+            try { m_CpuUsage[sample_index] = m_CpuCounter.NextValue() / (System.Environment.ProcessorCount * 100d); }
             catch { m_CpuUsage[sample_index] = 0; }
-
-            m_LastCpuUsage = m_CpuUsage[sample_index];
-            m_LastMemUsage = m_MemoryUsage[sample_index];
         }
 
+        /// <summary>
+        /// Resets performance counters used
+        /// </summary>
         void ResetPerformanceCounter()
         {
             if (!Monitorable())
                 return;
 
-            if (m_PerfCounter != null)
-                m_PerfCounter.Close();
+            if (m_CpuCounter != null)
+                m_CpuCounter.Close();
 
-            m_PerfCounter = new PerformanceCounter(
+            m_CpuCounter = new PerformanceCounter(
                 "Process",
                 "% Processor Time",
                 m_Process.ProcessName,
                 m_Process.MachineName);
         }
 
+        /// <summary>
+        /// Returns true if m_Process is monitor able
+        /// </summary>
         bool Monitorable()
         {
             try { return m_Process != null && !m_Process.HasExited; }
             catch { return false; }
         }
 
+        /// <summary>
+        /// Returns true if process has main window (false for Console processes)
+        /// </summary>
         bool HasMainWindow()
         {
             try { return Monitorable() && m_Process.MainWindowHandle != IntPtr.Zero; }
             catch { return false; }
         }
 
+        /// <summary>
+        /// Validates members before launching m_Process
+        /// </summary>
+        /// <returns>true on success</returns>
         bool Validate()
         {
             m_WaitTime      = Math.Abs(m_WaitTime);
             m_DelaySeconds  = Math.Abs(m_DelaySeconds);
-            m_ProcessPath   = m_ProcessPath.AsPath();
-            m_CrashScript   = m_CrashScript.AsPath();
-            m_StartScript   = m_StartScript.AsPath();
-            m_WorkingDir    = m_WorkingDir.AsPath();
+            m_ProcessPath   = m_ProcessPath.AsPath(Extensions.PathType.FilePath);
+            m_CrashScript   = m_CrashScript.AsPath(Extensions.PathType.FilePath);
+            m_StartScript   = m_StartScript.AsPath(Extensions.PathType.FilePath);
+            m_WorkingDir    = m_WorkingDir.AsPath(Extensions.PathType.FilePath);
 
             if (m_ProcessPath != string.Empty && !Path.IsPathRooted(m_ProcessPath))
                 m_ProcessPath = Path.GetFullPath(m_ProcessPath);
@@ -421,34 +488,23 @@
             return true;
         }
 
-        static void CallScript(string path)
+        /// <summary>
+        /// Executes a script
+        /// </summary>
+        /// <param name="script">path to the script</param>
+        /// <param name="cmd">command line arguments</param>
+        /// <param name="headless">launch headless?</param>
+        static void ExecuteScript(string script, string cmd, bool headless)
         {
-            if (String.IsNullOrWhiteSpace(path))
+            if (String.IsNullOrWhiteSpace(script))
                 return;
 
             using (Process process = new Process {
                 StartInfo = new ProcessStartInfo {
-                    FileName = path
-                }
-            })
-            {
-                process.Start();
-                process.WaitForExit();
-            }
-        }
-
-        static void ExecuteScript(string exe, string cmd)
-        {
-            if (String.IsNullOrWhiteSpace(exe) ||
-                String.IsNullOrWhiteSpace(cmd))
-                return;
-
-            using (Process process = new Process {
-                StartInfo = new ProcessStartInfo {
-                    FileName = exe,
+                    FileName = script,
                     Arguments = cmd,
-                    CreateNoWindow = true,
-                    UseShellExecute = true,
+                    CreateNoWindow = headless,
+                    UseShellExecute = headless,
                 }
             })
             {
@@ -457,6 +513,7 @@
             }
         }
 
+        //! @cond
         #region IDisposable Support
         private bool m_Disposed = false;
 
@@ -466,8 +523,8 @@
             {
                 if (disposing)
                 {
-                    if (m_PerfCounter != null)
-                        m_PerfCounter.Close();
+                    if (m_CpuCounter != null)
+                        m_CpuCounter.Close();
 
                     if (m_Process != null)
                         m_Process.Close();
@@ -479,9 +536,10 @@
 
         public void Dispose()
         {
-            Stop(ExecType.NORMAL);
+            Stop(ExecType.Normal);
             Dispose(true);
         }
         #endregion
+        //! @endcond
     }
 }
